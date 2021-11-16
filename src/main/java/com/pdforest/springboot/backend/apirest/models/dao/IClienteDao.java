1ab1530fae8c6af68752d2060c64a8539b5fae01
pdforest <pdforest@gmail.com>
2021-11-16 15:22:37 -0300

    Primer commit

---
+++ src/main/java/com/pdforest/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.pdforest.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
+
+import com.pdforest.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+}
