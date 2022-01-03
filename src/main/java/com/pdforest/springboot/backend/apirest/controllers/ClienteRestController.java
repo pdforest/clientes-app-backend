a3c1b78998daeb5018dba0b3321775c2dd097dbe
pdforest <pdforest@gmail.com>
2022-01-03 13:22:50 -0300

    Agrego en CORS el origen *.herokuapp.com

--- src/main/java/com/pdforest/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/pdforest/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -37,7 +37,7 @@ import com.pdforest.springboot.backend.apirest.models.entity.Region;
 import com.pdforest.springboot.backend.apirest.models.services.IClienteService;
 import com.pdforest.springboot.backend.apirest.models.services.IUploadFileService;
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "http://localhost:4200", "*.herokuapp.com"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
